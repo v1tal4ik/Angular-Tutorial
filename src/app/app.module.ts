bc4e8d7ec2ff87ec6a5555418ef6de6b894d86da
v1tal4ik <vitalii.suhodolskiy@gmail.com>
2020-09-21 10:15:11 +0300

    with router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoModule } from './todo/todo.module';
+import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+
+const appRoutes: Routes = [{ path: '', component: TodoComponent }];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TodoModule],
+  imports: [BrowserModule, TodoModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
